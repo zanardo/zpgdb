137349f10ef24fdd6bfd3afb9ca9858fcee55a9e
Antonio Zanardo <zanardo@gmail.com>
2017-10-08 21:07:03 -0300

    standarize quotes

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import re
 from setuptools import setup
 
 # argh!
-version = ''
-with open('zpgdb.py', 'r') as fp:
+version = ""
+with open("zpgdb.py", "r") as fp:
     version = re.search(r"^__VERSION__ = '(.+?)'$", fp.read(),
                         re.MULTILINE).group(1)
 if not version:
@@ -13,23 +13,23 @@ if not version:
 setup(
     name="zpgdb",
     version=version,
-    py_modules=['zpgdb'],
-    install_requires=['psycopg2>=2.7.0,<=2.7.999'],
+    py_modules=["zpgdb"],
+    install_requires=["psycopg2>=2.7.0,<=2.7.999"],
     package_data={
-        '': ['README.md', 'COPYING']
+        "": ["README.md", "COPYING"]
     },
-    author='Antonio Zanardo',
-    author_email='zanardo@gmail.com',
-    description='Library for simple PostgreSQL connection and transaction management',
-    license='BSD',
-    keywords='postgresql database transaction connection',
-    url='https://github.com/zanardo/zpgdb',
+    author="Antonio Zanardo",
+    author_email="zanardo@gmail.com",
+    description="Library for simple PostgreSQL connection and transaction management",
+    license="BSD",
+    keywords="postgresql database transaction connection",
+    url="https://github.com/zanardo/zpgdb",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Database',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Database",
     ],
 )
